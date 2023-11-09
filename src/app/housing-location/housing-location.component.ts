9d64fd2f21981b263ad131c281d34f2109b994e6
Borozan-Evghenii <borozan.eugen@gmail.com>
2023-11-09 07:32:28 +0200

    Am generat componenta HouseLocation pentru a vizualiza datele mock, am folosit directiva *ngFor pentru a vizualiza o lista de componente , am folosit decoratorul @Input pentur a descrie propsurile de intare în componenta HousingLocation

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HousingLocation} from "../housinglocation";
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    </section>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation
+  // Acest decorator descrie ce date urmează să primească componenta ca props prin tag
+  //<app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+  //[housingLocation] = denumirea propsului declarat cu ajutorul @Input după care urmează legătura cu date
+  // ce se conțin în clasa unde componenta este folosită adică în homeComponent
+}

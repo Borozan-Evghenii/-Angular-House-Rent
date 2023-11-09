59c5b059109ed49c4f43fe71e66468e9bf8c5404
Borozan-Evghenii <borozan.eugen@gmail.com>
2023-11-09 07:45:13 +0200

    Am generat un serviciu HouseLocationService care interacționează cu Componenta HouseLocation partajează datele despre casele disponibile, Am adăugat rute pentru aplicație în route.ts am descris routeConfig pentru aplicație am folosit routeLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HomeComponent} from "./home/home.component";
 import {HousingLocationComponent} from "./housing-location/housing-location.component";
+import {RouterModule} from "@angular/router";
 
 
 @Component({
@@ -8,18 +9,21 @@ import {HousingLocationComponent} from "./housing-location/housing-location.comp
   standalone: true,
   imports: [
       HomeComponent,
-      HousingLocationComponent
+      HousingLocationComponent,
+      RouterModule
   ],
-  template: `
-    <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-      </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
+    template: `
+        <main>
+            <a [routerLink]="['/']">
+                <header class="brand-name">
+                    <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+                </header>
+            </a>
+            <section class="content">
+                <router-outlet></router-outlet>
+            </section>
+        </main>
+    `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
